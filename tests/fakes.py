ed11655557babb9c0814520414a6f34cb602006e
yulgon <younggon5548@gmail.com>
2026-07-15 15:47:38 +0900

    [RED] add failing tests for AppController

--- tests/fakes.py
+++ tests/fakes.py
@@ -5,12 +5,16 @@ class FakeView:
         self._inputs = list(inputs or [])
         self.messages = []
         self.menus = []
+        self.status_bars = []
 
     def show_message(self, message):
         self.messages.append(message)
 
     def show_menu(self, title, options):
         self.menus.append((title, options))
+
+    def show_status_bar(self, registered_samples, total_stock, total_orders, waiting_lines):
+        self.status_bars.append((registered_samples, total_stock, total_orders, waiting_lines))
 
     def get_input(self, prompt):
         return self._inputs.pop(0)
