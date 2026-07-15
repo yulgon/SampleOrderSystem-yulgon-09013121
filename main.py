366860dbf38879f488851fdd5ec56f473c536d9b
yulgon <younggon5548@gmail.com>
2026-07-15 14:19:08 +0900

    [GREEN] wire 주문 (승인/거절) into the temporary main menu

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ def main():
     order_controller = OrderController(view)
 
     while True:
-        view.show_menu("메인 메뉴", [(0, "종료"), (1, "시료 관리"), (2, "시료 주문")])
+        view.show_menu(
+            "메인 메뉴",
+            [(0, "종료"), (1, "시료 관리"), (2, "시료 주문"), (3, "주문 (승인/거절)")],
+        )
         choice = view.get_input("메뉴 번호를 입력하세요: ")
 
         if choice == "0":
@@ -18,6 +21,8 @@ def main():
             sample_controller.run()
         elif choice == "2":
             order_controller.run_reserve()
+        elif choice == "3":
+            order_controller.run_approve_reject()
         else:
             view.show_message("잘못된 입력입니다.")
 
