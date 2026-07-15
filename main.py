e06c7e52f76373733f66b11b6dea6d1ceb550f2f
yulgon <younggon5548@gmail.com>
2026-07-15 14:47:41 +0900

    [GREEN] wire 생산 라인 into the temporary main menu

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from view.console_view import ConsoleView
 from controller.sample_controller import SampleController
 from controller.order_controller import OrderController
+from controller.production_controller import ProductionController
 
 
 def main():
     view = ConsoleView()
     sample_controller = SampleController(view)
     order_controller = OrderController(view)
+    production_controller = ProductionController(view)
 
     while True:
         view.show_menu(
             "메인 메뉴",
-            [(0, "종료"), (1, "시료 관리"), (2, "시료 주문"), (3, "주문 (승인/거절)")],
+            [
+                (0, "종료"),
+                (1, "시료 관리"),
+                (2, "시료 주문"),
+                (3, "주문 (승인/거절)"),
+                (4, "생산 라인"),
+            ],
         )
         choice = view.get_input("메뉴 번호를 입력하세요: ")
 
@@ -23,6 +31,8 @@ def main():
             order_controller.run_reserve()
         elif choice == "3":
             order_controller.run_approve_reject()
+        elif choice == "4":
+            production_controller.run()
         else:
             view.show_message("잘못된 입력입니다.")
 
